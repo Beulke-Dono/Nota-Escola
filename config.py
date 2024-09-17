c4873c58001a09521a5149155028f019907171cb
Beulke-Dono <arthurbeulke@gmail.com>
2024-09-16 22:31:40 -0400

    Teste

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'uma-chave-secreta'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = f"sqlite:///{os.path.join(basedir, 'instance', 'app.db')}"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
